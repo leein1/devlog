2b5a6e6273e47fcf19230bb637e33ad0fe936030
leeinwon <inwon.private@gmail.com>
2026-03-24 19:53:59 +0900

    프로젝트 생성

---
+++ src/test/java/kr/leeinwon/devlog/DevlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.leeinwon.devlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
