4eff7e9cc8ed15f1fc0f6768905953c2db0cd300
leeinwon <inwon.private@gmail.com>
2026-03-26 21:39:57 +0900

    feat:Category Entity 생성

---
+++ src/main/java/kr/leeinwon/devlog/entity/Category.java
@@ -0,0 +1,23 @@
+package kr.leeinwon.devlog.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true,length = 50)
+    private String name;
+
+    @Builder
+    private Category(String name) {
+        this.name = name;
+    }
+}
