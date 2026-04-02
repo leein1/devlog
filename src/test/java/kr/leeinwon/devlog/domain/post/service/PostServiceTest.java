ea64913671be4fc4a6117f7131faeed82fdcd07d
leeinwon <inwon.private@gmail.com>
2026-04-02 20:58:56 +0900

    feat: PostServiceTest 생성 및 게시글 생성 + 카테고리 미지정 게시글 생성 메소드 작성

---
+++ src/test/java/kr/leeinwon/devlog/domain/post/service/PostServiceTest.java
@@ -0,0 +1,121 @@
+package kr.leeinwon.devlog.domain.post.service;
+
+import jakarta.persistence.EntityManager;
+import kr.leeinwon.devlog.domain.category.entity.Category;
+import kr.leeinwon.devlog.domain.post.dto.PostRequest;
+import kr.leeinwon.devlog.domain.post.dto.PostResponse;
+import kr.leeinwon.devlog.domain.post.entity.Post;
+import kr.leeinwon.devlog.domain.post.repository.PostRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(PostServiceTest.class);
+    @Mock
+    PostRepository postRepository;
+
+    @Mock
+    EntityManager  entityManager;
+
+    @InjectMocks
+    PostService postService;
+
+    private Post createPost(Long id, String title, String content, Category category) {
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .category(category)
+                .build();
+
+        try{
+            var idField = Post.class.getDeclaredField("id");
+            idField.setAccessible(true);
+            idField.set(post, id);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return post;
+    }
+
+    private Category createCategory(Long id, String name) {
+        Category category = Category.builder()
+                .name(name)
+                .build();
+
+        try{
+            var idField = Category.class.getDeclaredField("id");
+            idField.setAccessible(true);
+            idField.set(category, id);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return category;
+    }
+
+    private PostRequest createPostRequest(String title, String content, Long categoryId) {
+
+        PostRequest postRequest = PostRequest.builder()
+                .title(title)
+                .content(content)
+                .categoryId(categoryId)
+                .build();
+        return postRequest;
+    }
+
+    @Test
+    void 게시글_생성_성공(){
+        //given
+        PostRequest postRequest = createPostRequest("제목", "내용", null);
+        Category category = createCategory(1L, "미분류");
+        Post post = createPost(1L, "제목", "내용",  category);
+
+        given(entityManager.getReference(Category.class, 1L)).willReturn(category);
+        given(postRepository.save(any(Post.class))).willReturn(post);
+        //when
+        PostResponse postResponse = postService.createPost(postRequest);
+
+        //then
+        assertThat(postResponse.getTitle()).isEqualTo("제목");
+        assertThat(postResponse.getContent()).isEqualTo("내용");
+        verify(postRepository).save(any(Post.class));
+    }
+
+    @Test
+    void 게시글_생성_카테고리_지정(){
+        //given
+        PostRequest postRequest = createPostRequest("제목", "내용", 2L);
+        Category category = createCategory(2L, "백준");
+        Post post = createPost(1L, "제목", "내용",  category);
+
+        given(entityManager.getReference(Category.class, 2L)).willReturn(category);
+        log.info("category name={}, id={}", category.getName(), category.getId());
+        given(postRepository.save(any(Post.class))).willReturn(post);
+        log.info("post title={}, content={}, categoryId={}"
+                , post.getTitle(), post.getContent(), post.getCategory().getId());
+
+        //when
+        PostResponse postResponse = postService.createPost(postRequest);
+
+        //then
+        assertThat(postResponse.getTitle()).isEqualTo("제목");
+        assertThat(postResponse.getContent()).isEqualTo("내용");
+        verify(postRepository).save(any(Post.class));
+    }
+
+}
