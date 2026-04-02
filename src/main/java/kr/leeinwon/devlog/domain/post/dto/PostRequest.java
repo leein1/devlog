e36ae167d6d9d11422a017ed61c5a06ad0875795
leeinwon <inwon.private@gmail.com>
2026-04-02 20:59:58 +0900

    feat: PostRequest @Allargs + @Builder 클래스에 적용

--- src/main/java/kr/leeinwon/devlog/domain/post/dto/PostRequest.java
+++ src/main/java/kr/leeinwon/devlog/domain/post/dto/PostRequest.java
@@ -1,11 +1,15 @@
 package kr.leeinwon.devlog.domain.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PostRequest {
 
     @NotBlank(message = "제목은 필수입니다")
