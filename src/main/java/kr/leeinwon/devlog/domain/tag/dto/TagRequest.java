98a3011c7a8b50db44a17161de997f964ff8a838
leeinwon <inwon.private@gmail.com>
2026-04-06 18:49:23 +0900

    feat (TagRequest): 클래스 생성

---
+++ src/main/java/kr/leeinwon/devlog/domain/tag/dto/TagRequest.java
@@ -0,0 +1,17 @@
+package kr.leeinwon.devlog.domain.tag.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TagRequest {
+
+    @NotBlank(message = "태그명은 필수입니다")
+    private String name;
+}
