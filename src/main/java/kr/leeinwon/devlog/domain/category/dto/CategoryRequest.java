bcc36db23df17e1e4afa6d00aa41d319114f5d66
leeinwon <inwon.private@icloud.com>
2026-04-27 16:46:36 +0900

    feat (CategoryRequest.class) : 카테고리 요청 Dto 생성

---
+++ src/main/java/kr/leeinwon/devlog/domain/category/dto/CategoryRequest.java
@@ -0,0 +1,13 @@
+package kr.leeinwon.devlog.domain.category.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CategoryRequest {
+
+    @NotBlank(message = "카테고리 이름은 필수입니다")
+    private String name;
+}
