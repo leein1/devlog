c62db83632d35dc21fcb1949dbeaa787f48569b1
leeinwon <inwon.private@icloud.com>
2026-05-21 21:00:08 +0900

    refactor (PostSeriesRepositoory) : findAllByPostId -> findAllByPostIdWithSeries로 시그니처 변경

--- src/main/java/kr/leeinwon/devlog/domain/series/PostSeriesRepository.java
+++ src/main/java/kr/leeinwon/devlog/domain/series/PostSeriesRepository.java
@@ -20,7 +20,7 @@ public interface PostSeriesRepository extends JpaRepository<PostSeries, Long> {
     Optional<PostSeries> findBySeriesIdAndPostId(Long seriesId, Long postId);
 
     @Query("select  ps from PostSeries  ps join fetch ps.series where ps.post.id = :postId")
-    List<PostSeries> findAllByPostId(@Param("postId") Long postId);
+    List<PostSeries> findAllByPostIdWithSeries(@Param("postId") Long postId);
 
 
     @Query("select ps from PostSeries ps join fetch ps.post where ps.series.id = :seriesId and ps.orderNum < :orderNum order by ps.orderNum desc")
