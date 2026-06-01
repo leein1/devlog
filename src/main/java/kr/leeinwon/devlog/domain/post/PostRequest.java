00de966000ad0ed2f9c06e99b8b3a44197336f7a
leeinwon <inwon.private@icloud.com>
2026-06-01 14:56:38 +0900

    feat (PostRequest.class) : 시리즈 아이디와 태그 리스트 필드추가

--- src/main/java/kr/leeinwon/devlog/domain/post/PostRequest.java
+++ src/main/java/kr/leeinwon/devlog/domain/post/PostRequest.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,4 +20,8 @@ public class PostRequest {
     private String content;
 
     private Long categoryId;
+
+    private Long seriesId;
+
+    private List<Long> tagIdList;
 }
