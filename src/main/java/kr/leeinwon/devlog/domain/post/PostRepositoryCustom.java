44d8f4161acf8b67dac02d16d08448a8ff214148
leeinwon <inwon.private@icloud.com>
2026-07-18 00:35:45 +0900

    feat (PostRepositoryCustom, impl) : 목록 커서 방식에서 오프셋으로 전환 + 상세보기에서 이전,이후 글 목록 보기 위한 findNearByPosts 메서드 추가

--- src/main/java/kr/leeinwon/devlog/domain/post/PostRepositoryCustom.java
+++ src/main/java/kr/leeinwon/devlog/domain/post/PostRepositoryCustom.java
@@ -1,8 +1,15 @@
 package kr.leeinwon.devlog.domain.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface PostRepositoryCustom {
 
-    List<Post> searchPosts(Long cusor, int size, Long categoryId, String tagName, String keyword);
+    List<Post> searchPostsOld(Long cursor, int size, Long categoryId, String tagName, String keyword);
+
+    Page<Post> searchPosts(Pageable pageable, Long categoryId, String tagName, String keyword);
+
+    List<Post> findNearbyPosts(Long id, int n);
 }
--- src/main/java/kr/leeinwon/devlog/domain/post/PostRepositoryCustomImpl.java
+++ src/main/java/kr/leeinwon/devlog/domain/post/PostRepositoryCustomImpl.java
@@ -3,11 +3,16 @@ package kr.leeinwon.devlog.domain.post;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.leeinwon.devlog.domain.tag.PostTagRepository;
 import kr.leeinwon.devlog.domain.tag.QPostTag;
 import kr.leeinwon.devlog.domain.tag.QTag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -15,9 +20,10 @@ import java.util.List;
 public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final PostTagRepository postTagRepository;
 
     @Override
-    public List<Post> searchPosts(Long cusor, int size, Long categoryId, String tagName, String keyword) {
+    public List<Post> searchPostsOld(Long cursor, int size, Long categoryId, String tagName, String keyword) {
 
         QPost post = QPost.post;
         QPostTag postTag = QPostTag.postTag;
@@ -25,13 +31,14 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 
         BooleanBuilder builder = new BooleanBuilder();
 
-        if(cusor != null){
-            builder.and(post.id.lt(cusor));
-        };
-        if(categoryId != null){
+        if (cursor != null) {
+            builder.and(post.id.lt(cursor));
+        }
+        ;
+        if (categoryId != null) {
             builder.and(post.category.id.eq(categoryId));
         }
-        if(keyword != null && !keyword.isBlank()){
+        if (keyword != null && !keyword.isBlank()) {
             builder.and(post.title.contains(keyword));
         }
 
@@ -43,7 +50,7 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom {
                 .orderBy(post.id.desc())
                 .limit(size);
 
-        if(tagName != null && !tagName.isBlank()){
+        if (tagName != null && !tagName.isBlank()) {
             query = query
                     .join(post.postTags, postTag)
                     .join(postTag.tag, tag)
@@ -51,5 +58,85 @@ public class PostRepositoryCustomImpl implements PostRepositoryCustom {
         }
 
         return query.fetch();
+    }
+
+    @Override
+    public Page<Post> searchPosts(Pageable pageable, Long categoryId, String tagName, String keyword) {
+        QPost post = QPost.post;
+        QPostTag postTag = QPostTag.postTag;
+        QTag tag = QTag.tag;
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (categoryId != null) {
+            builder.and(post.category.id.eq(categoryId));
+        }
+        if (keyword != null && !keyword.isBlank()) {
+            builder.and(post.title.contains(keyword));
+        }
+
+        var query = jpaQueryFactory
+                .selectDistinct(post)
+                .from(post)
+                .leftJoin(post.category).fetchJoin()
+                .where(builder)
+                .orderBy(post.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize());
+
+        if (tagName != null && !tagName.isBlank()) {
+            query = query
+                    .join(post.postTags, postTag)
+                    .join(postTag.tag, tag)
+                    .where(tag.name.eq(tagName));
+        }
+
+        List<Post> content = query.fetch();
+
+        var countQuery = jpaQueryFactory
+                .select(post.countDistinct())
+                .from(post)
+                .where(builder);
+
+        if (tagName != null && !tagName.isBlank()) {
+            countQuery = countQuery
+                    .join(post.postTags, postTag)
+                    .join(postTag.tag, tag)
+                    .where(tag.name.eq(tagName));
+        }
+
+        long total = countQuery.fetchOne();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+
+    //  N _+ 1  발생지점
+    // Category Join 존재하지 않음. searchPosts(), searchPostOld() 엔 leftJoin(...).fetchJoin() 존재
+    @Override
+    public List<Post> findNearbyPosts(Long id, int n) {
+
+        QPost post = QPost.post;
+
+        List<Post> before = jpaQueryFactory
+                .selectFrom(post)
+                .leftJoin(post.category).fetchJoin()
+                .where(post.id.lt(id))
+                .orderBy(post.id.desc())
+                .limit(n)
+                .fetch();
+
+        List<Post> after = jpaQueryFactory
+                .selectFrom(post)
+                .leftJoin(post.category).fetchJoin()
+                .where(post.id.gt(id))
+                .orderBy(post.id.asc())
+                .limit(n)
+                .fetch();
+
+        Collections.reverse(before);
+        before.addAll(after);
+
+        return before;
     }
 }
